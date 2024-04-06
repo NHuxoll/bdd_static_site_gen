260acdf43396b83b419339a63c20bc0c1a90645a
NHuxoll <nils-huxoll@web.de>
2024-04-06 13:40:15 +0200

    added tests for inline markdown

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -13,23 +13,95 @@ from textnode import (
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_delim_bold(self):
-        pass
+        node = TextNode("This is a text with a **bold** word", text_type_text)
+        new_nodes = split_nodes_delimiter([node],"**",text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This is a text with a ", text_type_text),
+                TextNode("bold", text_type_bold),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes
+        )
     def test_delim_bold_double(self):
-        pass
+        node = TextNode("This is a text with **two** words **bolded** not one", text_type_text)
+        new_nodes = split_nodes_delimiter([node],"**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This is a text with ", text_type_text),
+                TextNode("two", text_type_bold),
+                TextNode(" words ", text_type_text),
+                TextNode("bolded", text_type_bold),
+                TextNode(" not one", text_type_text),
+
+            ],
+            new_nodes
+        )
     def test_delim_bold_multi_word(self):
-        pass
+        node = TextNode("This is a text with **multiple words** in **bold** font", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+                [
+                    TextNode("This is a text with ", text_type_text),
+                    TextNode("multiple words", text_type_bold),
+                    TextNode(" in ", text_type_text),
+                    TextNode("bold", text_type_bold),
+                    TextNode(" font", text_type_text),
+                ],
+            new_nodes
+        )
 
     def test_delim_italic(self):
-        pass
-
+        node = TextNode("This is a text with a *italic* word", text_type_text)
+        new_nodes = split_nodes_delimiter([node],"*",text_type_italic)
+        self.assertListEqual(
+            [
+                TextNode("This is a text with a ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes
+        )
     def test_delim_italic_double(self):
-        pass
+        node = TextNode("This is a text with *two* words *italiced* not one", text_type_text)
+        new_nodes = split_nodes_delimiter([node],"*", text_type_italic)
+        self.assertListEqual(
+            [
+                TextNode("This is a text with ", text_type_text),
+                TextNode("two", text_type_italic),
+                TextNode(" words ", text_type_text),
+                TextNode("italiced", text_type_italic),
+                TextNode(" not one", text_type_text),
 
+            ],
+            new_nodes
+        )
     def test_delim_italic_multi_word(self):
-        pass
+        node = TextNode("This is a text with *multiple words* in *italic* font", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "*", text_type_italic)
+        self.assertListEqual(
+                [
+                    TextNode("This is a text with ", text_type_text),
+                    TextNode("multiple words", text_type_italic),
+                    TextNode(" in ", text_type_text),
+                    TextNode("italic", text_type_italic),
+                    TextNode(" font", text_type_text),
+                ],
+            new_nodes
+        )
 
     def test_delim_code(self):
-        pass
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes,
+        )
+
 
     def test_node_code(self):
         pass
