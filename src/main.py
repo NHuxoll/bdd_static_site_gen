f92a4ff6750f19cd089cbccb85424a070c25e540
Nils Huxoll <Nils-Huxoll@web.de>
2024-07-14 18:27:45 +0200

    finished

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 import os
 import shutil
-from textnode import TextNode
+from copy_to_public import copy_files_recursive
+from gencontent import generate_page, generate_pages_recursive
 
-def copy_static() -> None:
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+def main():
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    #shutil.rmtree()
-    pass
+    copy_files_recursive(dir_path_static, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+if __name__ == "__main__":
+    main()
